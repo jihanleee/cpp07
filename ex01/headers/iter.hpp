150e2952550811d377bbc80062ca6f647b3740e9
jihalee <jihalee@student.42.fr>
2024-03-27 00:40:37 +0100

    moved test functions to the other file

--- ex01/headers/iter.hpp
+++ ex01/headers/iter.hpp
@@ -2,14 +2,6 @@
 # define iter_HPP
 # include <string.h>
 
-template<typename T> void function(T &target){
-	std::cout<< ++target << std::endl;
-}
-
-void function2(int &target){
-	std::cout<< ++target << std::endl;
-}
-
 template<typename T> void iter(T *array, size_t length, void function(T &)) {
 	size_t i;
 
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -2,14 +2,23 @@
 #include <string>
 #include "iter.hpp"
 
+template<typename T>
+void tempFunction (T &target) {
+	std::cout<< ++target << std::endl;
+}
+
+void function2(int &target){
+	std::cout<< ++target << std::endl;
+}
+
 int main( void ) {
 	int a[] = {1, 2, 3, 4, 5};
 	char b[] = {'a', 'b', 'c', 'd', 'e'};
 	std::cout << "iter with a function template instance for int var\n";
-	iter(a, 5, function);
+	iter(a, 5, tempFunction);
 	std::cout << "iter with a function for int var\n";
 	iter(a, 5, function2);
 	std::cout << "iter with a function template instance of char var\n";
-	iter(b, 5, function);
+	iter(b, 5, tempFunction);
 	return 0;
 }
